0419b9211198acc1b1d3a9deb0655023b4a1274f
Shivam Aggarwal <shivama205@gmail.com>
2023-08-31 22:21:02 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -101,8 +101,10 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Provide the response in following JSON format:  [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise return an empty array.
-- Write the comment in GitHub Markdown format.
-- Use the given description only for the overall context and only comment the code.
+- Provide conceptual knowledge in the comments if necessary. 
+- Ensure endpoints follow RESTful architecture. 
+- Write the comment in GitHub Markdown format. 
+- Use the given description only for the overall context and only comment the code. 
 - IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${
